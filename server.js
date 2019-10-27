f40ddb2fbaa1510c3153559dd2c4b16ad63e38a0
Katherine Ray <katherine.ray@me.com>
2019-10-27 16:05:35 -0500

    npm installs, var require setup srver.js, create route.js

---
+++ server.js
@@ -0,0 +1,8 @@
+var express = require("express");
+var exphbs = require('express-handlebars');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var mysql = require('mysql');
+
+var app = express();
+
